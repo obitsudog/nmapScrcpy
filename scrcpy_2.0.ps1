d4259a86bfe3f8e7f65a1881f1de12dd17afd908
obitsudog <103219181+obitsudog@users.noreply.github.com>
2022-10-28 19:57:16 -0400

    Add files via upload

---
+++ scrcpy_2.0.ps1
@@ -0,0 +1,7 @@
+$nmap = (& 'C:\Program Files (x86)\nmap-7.92\nmap' -Pn -p 30000-49999 10.10.10.3)
+$num = 30000..49999
+$port = $nmap.Split("{/}") | Select-String $num
+adb connect 10.10.10.3:$port
+& 'C:\Program Files\scrcpy\scrcpy' --max-size 768
+# <alt> + <r> to rotate
+adb kill-server
