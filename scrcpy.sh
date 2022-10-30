0f03f7acc58f9f7abb75c77ca5997810c61bb484
obitsudog <103219181+obitsudog@users.noreply.github.com>
2022-10-29 21:50:48 -0400

    Add files via upload

---
+++ scrcpy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Add platform-tools folder to $PATH:
+#nano ~/.zshrc
+# Add export PATH="$HOME/Downloads/platform-tools:$PATH" to end of file & save
+
+# Pair on first connection:
+#/home/colforbin/Downloads/platform-tools/adb start-server
+# On phone, Developer Options>Wireless debugging>Pair device with pairing code
+#/home/colforbin/Downloads/platform-tools/adb pair 10.10.10.3:$pairingport
+
+sudo IP=10.10.10.3 bash -c '/home/colforbin/Downloads/platform-tools/adb connect $IP:$(sudo nmap $IP -p 30000-49999 -Pn -T5 -oG - --defeat-rst-ratelimit | awk "/open/{print \$5}" | cut -d/ -f1)' && scrcpy -m 768
