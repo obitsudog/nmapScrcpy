48f8ff5d67c3fa0ec5d10cf6c99a50db5fd08385
obitsudog <103219181+obitsudog@users.noreply.github.com>
2022-10-29 20:00:49 -0400

    Update scrcpy.ps1

--- scrcpy.ps1
+++ scrcpy.ps1
@@ -1,4 +1,4 @@
-$nmap = (& 'C:\Program Files (x86)\nmap-7.92\nmap' -Pn -p 30000-49999 192.168.1.10)
+$nmap = (& 'C:\Program Files (x86)\nmap-7.92\nmap' -Pn -T5 -p 30000-49999 192.168.1.10)
 $num = 30000..49999
 $port = $nmap.Split("{/}") | Select-String $num
 adb connect 192.168.1.10:$port
