84f62a4c2acdf3d3e2989bc287e0425de4de98c4
obitsudog <103219181+obitsudog@users.noreply.github.com>
2022-10-30 18:35:31 -0400

    Update scrcpy.sh

--- scrcpy.sh
+++ scrcpy.sh
@@ -5,9 +5,9 @@
 # Add export PATH="$HOME/Downloads/platform-tools:$PATH" to end of file & save
 
 # Pair on first connection:
-#/home/colforbin/Downloads/platform-tools/adb start-server
+#~/Downloads/platform-tools/adb start-server
 # On phone, Developer Options>Wireless debugging>Pair device with pairing code
-#/home/colforbin/Downloads/platform-tools/adb pair 10.10.10.3:$pairingport
+#~/Downloads/platform-tools/adb pair 10.10.10.3:$pairingport
 
 # Code sourced from https://maxammann.org/posts/2022/01/android11-adb-automatic/
-sudo IP=10.10.10.3 bash -c '/home/colforbin/Downloads/platform-tools/adb connect $IP:$(sudo nmap $IP -p 30000-49999 -Pn -T5 -oG - --defeat-rst-ratelimit | awk "/open/{print \$5}" | cut -d/ -f1)' && scrcpy -m 768
+sudo IP=10.10.10.3 bash -c '~/Downloads/platform-tools/adb connect $IP:$(sudo nmap $IP -p 30000-49999 -Pn -T5 -oG - --defeat-rst-ratelimit | awk "/open/{print \$5}" | cut -d/ -f1)' && scrcpy -m 768
